35a2adbca7da4cb66cc6b434703dd09af2ffbb64
Ian Odhiambo <you@example.com>
2018-10-22 00:12:47 +0300

    new comment

--- hood/forms.py
+++ hood/forms.py
@@ -1,7 +1,20 @@
 from django import forms
 from .models import Profile, Hood, Business, Post
 
+
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ["name",]
+        fields = ["name", "prof_pic", "bio", "email"]
+
+
+class Hoodform(forms.ModelForm):
+    class Meta:
+        model = Hood
+        fields = ["name", "image", "description", "occupants_count"]
+
+
+class BusinessForm (forms.ModelForm):
+    class Meta:
+        model = Business
+        fields = ["name", "b_owner", "contac", "email", "description"]
