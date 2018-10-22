172ab5e5cb928bf4fa5217e41ffa3caf60ae8615
Ian Odhiambo <you@example.com>
2018-10-22 11:40:02 +0300

    added admin site

--- hood/views.py
+++ hood/views.py
@@ -2,20 +2,23 @@ from django.shortcuts import redirect, render, get_object_or_404
 from .forms import ProfileForm, Hoodform, BusinessForm
 from django.contrib.auth.decorators import login_required
 from .models import Hood
+from urllib import request
+
 # Create your views here.
 
 
 def home(request):
     current_user = request.user
-
+    print(current_user)
     if request.method == 'POST':
-        form = Hoodform(request.POST)
-        if form.is_valid():
-            upload = form.save(commit=False)
+        formhood = Hoodform(request.POST)
+        if formhood.is_valid():
+            upload = formhood.save(commit=False)
             upload.admin = current_user
             upload.save()
             return redirect('home')
     else:
+
         formhood = Hoodform()
     welcome = "welcome to the home page"
 
